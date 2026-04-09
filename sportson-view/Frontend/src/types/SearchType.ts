1a164ffb9f717cb9b6788c883dd36960634eeacb
Charlie Carlegrund <charlie_carlegrund@hotmail.com>
2026-04-09 10:35:30 +0200

    Lagt till Searchbar med css i ManualsPage. Påbörjat struktur av searchservice för att göra searchbar funktionell, osäker om det är rätt väg att gå?

---
+++ sportson-view/Frontend/src/types/SearchType.ts
@@ -0,0 +1,14 @@
+// import type { Pdf } from "./PdfType";
+// import type { YouTubeVideo } from "./YoutubeType";
+// import type { Article } from "./ArticleType";
+
+// export type SearchResult =
+//   | { type: "pdf"; data: Pdf }
+//   | { type: "YoutubeVideo"; data: YouTubeVideo }
+//   | { type: "Article"; data: Article };
+
+// export type SearchContextType = {
+//   results: SearchResult[];
+//   isLoading: boolean;
+//   search: (query: string, signal?: AbortSignal) => void;
+// };
