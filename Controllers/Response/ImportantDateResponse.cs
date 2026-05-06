f1f2b00814d73450ae9cfed6762afe9f1d3241f4
Suthida Damchum <suthida.damchum@hotmail.com>
2026-05-06 09:21:29 +0200

    Delete Controllers directory

--- Controllers/Response/ImportantDateResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using SportsonView.API.Dto;
-
-namespace SportsonView.API.Controllers.Response
-{
-    public class ImportantDateResponse
-    {
-        public List<ImportantDateDto> ImportantDates { get; set; }
-    }
-}
