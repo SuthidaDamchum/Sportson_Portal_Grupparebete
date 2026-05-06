d20fbf703cf23075f6a7c0c0bf2952055d0a583d
Suthida Damchum <suthida.damchum@hotmail.com>
2026-05-06 09:21:55 +0200

    Delete Core directory

--- Core/Interfaces/IImportantDatesService.cs
+++
@@ -1,9 +0,0 @@
-﻿using SportsonView.API.Dto;
-
-namespace SportsonView.API.Core.Interfaces
-{
-    public interface IImportantDatesService
-    {
-        Task<List<ImportantDateDto>> GetAllImportantDatesAsync();
-    }
-}
